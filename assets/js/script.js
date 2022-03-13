79c2c120a77f60fbf2beb51bee6d8d5919748917
svankampen <svankampen@croghan.co>
2022-03-13 09:55:36 +0000

    Function: update JS File with const, building html page with javascript instead of writing each item in html, single div added to html in place of full layout

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const introDisplayMessage = document.createElement('h2')
+const userChoiceDisplay = document.createElement('h2')
+const computerChoiceDisplay = document.createElement('h2')
+const resultDisplay = document.createElement('h2')
+const playArea = document.getElementById('play_area')
+playArea.append(introDisplayMessage, userChoiceDisplay, computerChoiceDisplay, resultDisplay)
+
