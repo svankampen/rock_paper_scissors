a9131997e6c9080f2ddc5547ee93adf80631c7c6
svankampen <svankampen@croghan.co>
2022-03-13 11:52:47 +0000

    Function: add hover effect to button seclection, update to title in index.html. CSS: update with curser effect to clickable pointer for user feedback

---
+++ assets/js/scipt.js
@@ -0,0 +1,7 @@
+const userChoiceDisplay = document.createElement('h2') // create a display for the user of what they chose
+const robotChoiceDisplay = document.createElement('h2') // create a display for the robot of what they chose
+const resultChoiceDisplay = document.createElement('h2') // the result display of who wins the game
+const playArea = document.querySelector('#play_area') // reference stackoverflow forum for selecting via query
+playArea.append (userChoiceDisplay, robotChoiceDisplay, resultChoiceDisplay)
+
+
