58fd9d6151a05e7f01db536a41d95771bd4e1e22
svankampen <svankampen@croghan.co>
2022-03-18 12:16:09 +0000

    Function: set up eventlisterner for the DOM to complete loading before running any other functions. Set up constants for the computer choice, user choice and results. Set up possible outcomes and CHOICES.

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,7 +1,33 @@
-const userChoiceDisplay = document.createElement('h2') // create a display for the user of what they chose
-const robotChoiceDisplay = document.createElement('h2') // create a display for the robot of what they chose
-const resultChoiceDisplay = document.createElement('h2') // the result display of who wins the game
-const playArea = document.querySelector('#play_area') // reference stackoverflow forum for selecting via query
-playArea.append (userChoiceDisplay, robotChoiceDisplay, resultChoiceDisplay)
+document.addEventListener("DOMContentLoaded", => e {
+    let buttons = document.getElementsByTagName("button");
+})
+
+const yourChoice = document.getElementById('your_choice')
+const computerChoice = document.getElementById('computer_choice')
+const result = document.getElementById('result')
+
+const CHOICES = [
+    {
+        id: 'rock',
+        name: 'Rock',
+        winsOver: 'scissors',
+        imageSrc: '<path_to_image>'
+    },
+    {
+        id: 'paper',
+        name: 'Paper',
+        winsOver: 'rock',
+        imageSrc: '<path_to_image>'
+    },
+    {
+        id: 'scissors',
+        name: 'Scissors',
+        winsOver: 'paper',
+        imageSrc: '<path_to_image>'
+    },
+]
+
+
+
 
 
