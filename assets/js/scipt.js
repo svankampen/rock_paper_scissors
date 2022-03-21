4e9d435776ab89a4ff3454ec8cfa5ab1cbf49f31
svankampen <svankampen@croghan.co>
2022-03-21 20:05:05 +0000

    Function: add correct syntax for DomcontentLoaded, run test re load up. Function: Declare const for user choice, computer choice, results. Function: Add evet listener for click on each button by user, inner html display with user choice.

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,31 +1,39 @@
-document.addEventListener("DOMContentLoaded", => e {
+document.addEventListener("DOMContentLoaded", () => {
     let buttons = document.getElementsByTagName("button");
 })
 
-const yourChoice = document.getElementById('your_choice')
-const computerChoice = document.getElementById('computer_choice')
-const result = document.getElementById('result')
+const yourChoiceDisplay = document.getElementById('your_choice')
+const computerChoiceDisplay = document.getElementById('computer_choice')
+const resultDisplay = document.getElementById('result')
+const possibleSelections = document.querySelectorAll('button') // can pick this out by class name also
 
-const CHOICES = [
-    {
-        id: 'rock',
-        name: 'Rock',
-        winsOver: 'scissors',
-        imageSrc: '<path_to_image>'
-    },
-    {
-        id: 'paper',
-        name: 'Paper',
-        winsOver: 'rock',
-        imageSrc: '<path_to_image>'
-    },
-    {
-        id: 'scissors',
-        name: 'Scissors',
-        winsOver: 'paper',
-        imageSrc: '<path_to_image>'
-    },
-]
+let yourChoice
+let robotChoice
+
+possibleSelections.forEach(possibleSelection => possibleSelection.addEventListener('click', (e) => {
+    yourChoice = e.target.id
+    yourChoiceDisplay.innerHTML = yourChoice
+    getComputerChoice()
+}))
+
+function getComputerChoice() {
+    const randomNumberComputer = Math.floor (Math.random() * possibleSelections.length) + 1
+    
+    if (randomNumberComputer === 1) {
+        robotChoice = 'rock'
+    }
+    if (randomNumberComputer === 2) {
+        robotChoice = 'paper'
+    }
+    if (randomNumberComputer === 3) {
+        robotChoice = 'scissors'
+    }
+
+    computerChoiceDisplay.innerHTML = robotChoice
+}
+
+
+
 
 
 
